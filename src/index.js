1fe201016a2eb3f7e0f49b987c8f77e479140045
Francisco Marcano <fmarcano@brightgauge.com>
2018-01-19 11:22:18 -0500

    Add create redux store, add devtools, and provide store to App

--- src/index.js
+++ src/index.js
@@ -4,5 +4,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+
+const rootReducer = combineReducers({})
+const store = createStore(
+	rootReducer,
+	{},
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+)
+
 registerServiceWorker();
